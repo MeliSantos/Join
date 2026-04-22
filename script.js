774b93f11d263a5c86a14fc8adf026e4edc47b90
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-04-22 21:36:20 +0200

    User data can be entered on the "sign-in" page and then added to the database. The system checks whether all fields are filled in and whether the checkbox is selected.

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("Name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const termsCheckbox = document.getElementById("option1").checked;
+
+    if (!termsCheckbox) {
+        termsError.textContent = "Bitte akzeptieren Sie die Privacy Policy, um fortzufahren.";
+        return;
+    } else {
+        termsError.textContent = "";
+    }
+
+    const user = {
+        name: name,
+        email: email,
+        initials: name.charAt(0),
+        password: password
+    };
+
+    sendToFirebase(user);
+});
+
+function sendToFirebase(user) {
+    fetch("https://join-45b16-default-rtdb.europe-west1.firebasedatabase.app/users.json", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log("Neue ID:", data.name);
+            form.reset();
+        });
+}
